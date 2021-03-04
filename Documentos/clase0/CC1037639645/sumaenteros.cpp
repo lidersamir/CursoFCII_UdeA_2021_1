0baac64a0efb17b3d4386e4518190892fdcbe2fe
tritio1995 <73112694+tritio1995@users.noreply.github.com>
2021-03-04 07:55:51 -0500

    Tarea0

---
+++ Documentos/clase0/CC1037639645/sumaenteros.cpp
@@ -0,0 +1,26 @@
+//Estefania Uribe Isaza -- 1037639645
+
+//Clase:Un conjunto de atributos y métodos que permiten definir un conjunto especifico y la operaciones que pueden realizarse con este.
+
+//POO:Un objeto es una instancia de la clase, entonces la programación orientada a objetos es una técnica que permite separar mediante clases los atributos y los métodos, haciendo más optima la implementación del código y como plus evita que los usuarios sean expuesto a la "caja negra" del código lo cuál es muy util a la hora de programar comercialmente
+
+
+//Codigo
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  int a, b, suma;   //Definicion de variables
+  cout <<"\n ingrese el primer numero a: ";
+  cin>>a;
+  cout <<"\n ingrese el segundo numero b: ";
+  cin>>b;
+  suma=a+b;
+  cout <<"\n la suma de " <<a<< "+" <<b<<" es "<<suma;
+  cout <<"\n \n";
+  return 0;
+}
+  
