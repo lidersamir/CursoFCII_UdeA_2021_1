0918f827692d2c5b841fdf9da194b2dca621bf27
MJDM23 <mjose.dominguez@udea.edu.co>
2021-03-16 21:29:42 -0500

    Segunda tarea

---
+++ Documentos/Seguimiento1/CC1152208162/tarea_2main.cpp
@@ -0,0 +1,9 @@
+#include "sine.h" //Incluye la definición de la clase 
+
+int main()
+{ //Implementación de la clase
+  Sine seno; //Se define un objeto de perteneciente a la clase
+  seno.Resultado();
+  
+  return 0; 
+} 
