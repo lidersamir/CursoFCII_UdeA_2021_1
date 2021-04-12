07a15e6d4b7d5fc2214ec9fa76e2ec848b91e1b8
ndrsbtncr <candres.betancur@udea.edu.co>
2021-04-11 19:30:37 -0500

    comentarios agregados en el pinto 1

--- Documentos/Parcial1/CC1038337471/Parcial1/punto1/complejo.cpp
+++ Documentos/Parcial1/CC1038337471/Parcial1/punto1/complejo.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 using namespace std;
 
 class complejo{
@@ -11,21 +12,21 @@ private:
   double parteImaginaria;
 
 public:
-  complejo(double,double);
-  complejo();
-  double asignarDatos();
-  double obtenerParteReal();
+  complejo(double,double); // constructor que inicializa las variables e introduce los complejos
+  complejo(); // Usamos un nuevo constructor para acceder desde el loop main() y hacer las operaciones
+  double asignarDatos(); // Tuve problemas para usar esta funcion
+  double obtenerParteReal(); 
   double obtenerParteImaginaria();
   void sumaComplejos();
   void restaComplejos();
-  void imprimaComplejos();
+  void imprimaComplejos(); // Tuve inconvenientes para extraer los complejos con una funcion extra.
 };
 complejo::complejo(){
   cout<<"HAGAMOS OPERACIONES CON COMPLEJOS"<<endl;
   cout<<"---------------------------------------"<<endl;
   cout<<"Los complejos ingresados son"<<endl;
 }
-complejo::complejo(double x, double y){
+complejo::complejo(double x, double y){ //Inicializa las variables
   parteReal=x;
   parteImaginaria=y;
   cout<<parteReal<<"+"<<parteImaginaria<<"i"<<endl;
@@ -36,7 +37,7 @@ double complejo::obtenerParteReal(){
 double complejo::obtenerParteImaginaria(){
   return parteImaginaria;
 }
-void complejo::sumaComplejos(){
+void complejo::sumaComplejos(){ //suma los numeros complejos
   //  asignarDatos();
   complejo c1(1,2);
   complejo c2(3,4);
@@ -48,7 +49,7 @@ void complejo::sumaComplejos(){
   cout<<"---------------------------------------------------"<<endl;
   
 }
-void complejo::restaComplejos(){
+void complejo::restaComplejos(){ //resta los numeros complejos
   complejo c1(1,2);
   complejo c2(3,4);
   
